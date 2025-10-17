e35728511664ad08b3e7d7882b5975ba34dbfaea
jam0824 <session_fixation@hotmail.co.jp>
2025-10-17 11:07:34 +0900

    イベント用の追加

--- run_voicevox.bat
+++ run_voicevox.bat
@@ -1,4 +1,4 @@
 start "" D:\download\tools\VOICEVOX\vv-engine\run.exe --host 0.0.0.0 --port 50021
 cd /d D:\codes\halo
 start npx -y @playwright/mcp@latest --host 0.0.0.0 --port 8931
-start ngrok http --domain=prevalid-unacrimoniously-leigh.ngrok-free.app 8931
+start ngrok http --domain=prevalid-unacrimoniously-leigh.ngrok-free.app 50021
